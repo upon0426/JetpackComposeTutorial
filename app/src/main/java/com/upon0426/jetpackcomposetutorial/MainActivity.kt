862688684df2028df63a5a1d916bd7a5cab85a2f
upon0426 <karasawa.yui@gmail.com>
2021-02-27 21:10:52 +0900

    blank activity

---
+++ app/src/main/java/com/upon0426/jetpackcomposetutorial/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.upon0426.jetpackcomposetutorial
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.upon0426.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
