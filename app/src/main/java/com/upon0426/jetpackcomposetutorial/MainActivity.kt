639abe57e2ed0d1cde68d2a67ac7638af7ebfca9
upon0426 <karasawa.yui@gmail.com>
2021-02-27 21:55:12 +0900

    add Colomn

--- app/src/main/java/com/upon0426/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/upon0426/jetpackcomposetutorial/MainActivity.kt
@@ -3,12 +3,10 @@ package com.upon0426.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.upon0426.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +18,12 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     fun NewsStory() {
-        Text("A day in Shark Fin Cove")
-        Text("Davenport, California")
-        Text("December 2018")
+        Column {
+            Text("A day in Shark Fin Cove")
+            Text("Davenport, California")
+            Text("December 2018")
+        }
+
     }
 
     @Preview
