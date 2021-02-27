45925ddf0faa9b30a8c681e8c18cb120ebb2231a
upon0426 <karasawa.yui@gmail.com>
2021-02-27 22:30:20 +0900

    added Materal Theme

--- app/src/main/java/com/upon0426/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/upon0426/jetpackcomposetutorial/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,25 +26,26 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     fun NewsStory() {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.header),
-                contentDescription = null,
-                modifier = Modifier
-                    .height(180.dp)
-                    .fillMaxWidth()
-                    .clip(shape = RoundedCornerShape(4.dp)),
-                contentScale = ContentScale.Crop
-            )
-            Spacer(modifier = Modifier.padding(8.dp))
+        MaterialTheme {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.header),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(180.dp)
+                        .fillMaxWidth()
+                        .clip(shape = RoundedCornerShape(4.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
 
-            Text("A day in Shark Fin Cove")
-            Text("Davenport, California")
-            Text("December 2018")
+                Text("A day in Shark Fin Cove")
+                Text("Davenport, California")
+                Text("December 2018")
+            }
         }
-
     }
 
     @Preview
