392200ee27a59975cd78db439d7d5ec4365364d2
upon0426 <karasawa.yui@gmail.com>
2021-02-27 21:56:51 +0900

    add modifier and padding

--- app/src/main/java/com/upon0426/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/upon0426/jetpackcomposetutorial/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +21,9 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     fun NewsStory() {
-        Column {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
             Text("A day in Shark Fin Cove")
             Text("Davenport, California")
             Text("December 2018")
